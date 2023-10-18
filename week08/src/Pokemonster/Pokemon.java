3359cf757cecbd4a5bc800ff87ad54dfe8ce5406
hojun13 <wjsghwns304@g.shingu.ac.kr>
2023-10-18 15:25:45 +0900

    week08 v44

--- week08/src/Pokemonster/Pokemon.java
+++ week08/src/Pokemonster/Pokemon.java
@@ -1,6 +1,5 @@
 package Pokemonster;
 
-import java.util.ArrayList;
 import java.util.List;
 
 //public final class Pokemonster.Pokemon {
@@ -12,10 +11,10 @@ public abstract class Pokemon {
     protected int attackRate;
     protected int defenceRate;
 
-//    protected String[] skills;  // 3가지 skill 추가 예정
-//    protected int[] specialAttackRate;
-    protected List<String> skills = new ArrayList<>();
-    protected List<Integer> specialAttackRate = new ArrayList<>();
+    //    protected String[] skills;
+    //    protected int[] specialAttackRate;
+    protected List<String> skills;
+    protected List<Integer> specialAttackRate;
     private static int pokemonCount = 0;  // 클래스(정적) 변수
 
     Flyable flyable;  // 연관 관계
@@ -94,8 +93,9 @@ public abstract class Pokemon {
     //public void attack(Pokemonster.Pokemon targetPokemon, String skill){
     public void attack(Pokemon targetPokemon, int skillNumber){
         //System.out.println(this.name +"이(가) " + targetPokemon.name + "에게 "+ skill +" 공격 시전!");
-        System.out.println(this.name +"이(가) " + targetPokemon.name + "에게 "+ this.skills.get(skillNumber - 1) +" 공격 시전!");
-        int temporaryAttackRate = (this.attackRate + this.specialAttackRate.get(skillNumber - 1)) - targetPokemon.defenceRate;
+        //System.out.println(this.name +"이(가) " + targetPokemon.name + "에게 "+ this.skills[skillNumber-1] +" 공격 시전!");
+        System.out.println(this.name +"이(가) " + targetPokemon.name + "에게 "+ this.skills.get(skillNumber-1) +" 공격 시전!");
+        int temporaryAttackRate = (this.attackRate + this.specialAttackRate.get(skillNumber-1)) - targetPokemon.defenceRate;
         if(temporaryAttackRate < 0)
             temporaryAttackRate = 0;
         targetPokemon.hp = targetPokemon.hp - temporaryAttackRate;
