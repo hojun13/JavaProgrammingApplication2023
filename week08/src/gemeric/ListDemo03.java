6e021247c64c1eb9d35a1806637e4dae47c3252c
hojun13 <wjsghwns304@g.shingu.ac.kr>
2023-10-18 14:12:43 +0900

    week08 v40

---
+++ week08/src/gemeric/ListDemo03.java
@@ -0,0 +1,16 @@
+package gemeric;
+
+
+
+import Pokemonster.*;
+
+import java.util.ArrayList; //
+import java.util.List;      // built in
+
+public class ListDemo03 {
+    public static void main(String[] args) {
+        List<String> skills = new ArrayList<>();
+        List<Integer> specialAttackRate = new ArrayList<>();
+
+    }
+}
